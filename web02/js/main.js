4e9910e3abbb3c4fc7605f2e391dffce1415e887
Ohdongyub <61488572+Ohdongyub@users.noreply.github.com>
2021-07-23 14:17:19 +0900

    Add files via upload

---
+++ web02/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $("#Wrap").fullpage({
+        navigation: true,
+        afterLoad: function(origin, destination, direction){
+            $(".section").eq(destination.index).addClass("on").siblings().removeClass("on");
+        },
+    });
+});
