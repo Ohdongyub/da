f85085fc40bf7dd6b624c9358ee069cdd33181f9
Ohdongyub <61488572+Ohdongyub@users.noreply.github.com>
2021-07-23 15:42:35 +0900

    Delete web02 directory

--- web02/js/main.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-    $("#Wrap").fullpage({
-        navigation: true,
-        afterLoad: function(origin, destination, direction){
-            $(".section").eq(destination.index).addClass("on").siblings().removeClass("on");
-        },
-    });
-});
